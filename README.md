60fab245128464c268acabb89ae09f60ccde7ad6
Tianhao Li <ysihaoy@gmail.com>
2016-08-18 21:06:52 +1000

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-app-egghead
+
+`$ npm install react`
+`$ npm install react-dom`
+`$ npm install babel-core babel-loader babel-preset-es2015 babel-preset-react --save-dev`
+`$ npm install webpack -g`
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './app/components/Main.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './public',
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
